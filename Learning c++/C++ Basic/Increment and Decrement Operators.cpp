92f0a1c978d4aab82596754043b6396d0c5fb9ae
Ashiful Alam <ashiful.shuvo@northsouth.edu>
2023-07-30 03:09:12 +0600

    Arithmetic Operators Increment and Decrement Operators

---
+++ Learning c++/C++ Basic/Increment and Decrement Operators.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Ashiful Alam on 30/7/23.
+//
+
+
+/*
+#include <iostream>
+using namespace std;
+
+int main (){
+    int a , b;
+    int aIN,bDE;
+    cout << "Enter value for a:" << endl;
+    cin >> a ;
+    cout << "Enter value for b:" << endl;
+    cin >> b;
+
+    aIN= a++;
+    cout<< "a after increment " << aIN << endl;
+
+    bDE = --b;
+    cout<< "b after decrement " <<bDE << endl;
+
+
+}*/
